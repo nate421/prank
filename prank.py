6affa4ac91ed14ceebf7543de15a29bc6ea2637d
nate421 <greg_julie@roadrunner.com>
2016-10-13 10:29:08 -0700

    feat: current working directory

--- prank.py
+++ prank.py
@@ -1,10 +1,13 @@
 import os
 
-print('test')
-
 def rename_files():
 
      file_list = os.listdir("./files")
-     print(file_list)
+    #  print(file_list)
+     saved_path = os.getcwd()
+     print("Current Working Directory is "+ saved_path)
+
+    #  for file_name in file_list:
+    #      os.rename(file_name, file_name.translate(None, "0123456789"))
 
 rename_files()
